506fe36d1db62c163bab5f7d42037982ea227ec9
Kim Mattiko <amberlie@rocketmail.com>
2015-08-15 20:54:52 -0400

    Fixed the style for contacts and changed an icon to look better

--- app/js/php/contact_me.php
+++ app/js/php/contact_me.php
@@ -4,11 +4,11 @@ if($_POST)
 	//check if its an ajax request, exit if not
     if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
         die();
-    } 
-	
-	$to_Email   	= "myemail@gmail.com"; //Replace with recipient email address
-	$subject        = ' '; //Subject line for emails
-	
+    }
+
+	$to_Email   	= "amberlie@rocketmail.com"; //Replace with recipient email address
+	$subject      = 'Contact Me'; //Subject line for emails
+
 	//check $_POST vars are set, exit if any missing
 	if(!isset($_POST["userName"]) || !isset($_POST["userEmail"])  || !isset($_POST["userMessage"]))
 	{
@@ -19,7 +19,7 @@ if($_POST)
 	$user_Name        = filter_var($_POST["userName"], FILTER_SANITIZE_STRING);
 	$user_Email       = filter_var($_POST["userEmail"], FILTER_SANITIZE_EMAIL);
 	$user_Message     = filter_var($_POST["userMessage"], FILTER_SANITIZE_STRING);
-	
+
 	//additional php validation
 	if(strlen($user_Name)<4) // If length is less than 4 it will throw an HTTP error.
 	{
@@ -33,10 +33,10 @@ if($_POST)
 	}
 	if(strlen($user_Message)<5) //check emtpy message
 	{
-		header('HTTP/1.1 500 Too short message! Please enter something.');
+		header('HTTP/1.1 500 Your message is too short, please add something.');
 		exit();
 	}
-	
+
 //proceed with PHP email.
     $headers = "Content-type:text/html;charset=UTF-8" . "\r\n" .
             "From: " . $user_Email . "\r\n" .
@@ -44,14 +44,14 @@ if($_POST)
             "X-Mailer: PHP/" . phpversion();
 
     $sentMail = mail($to_Email, $subject, $user_Message . '  -' . $user_Name, $headers);
-	
+
 	if(!$sentMail)
 	{
 		header('HTTP/1.1 500 Could not send mail! Sorry..');
 		exit();
 	}else{
 		echo 'Hi '.$user_Name .', Thank you for your email! ';
-		echo 'Your email has bee sand.';
+		echo 'Your email has been sent.';
 	}
 }
 ?>
